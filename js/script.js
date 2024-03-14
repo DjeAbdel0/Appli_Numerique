44f4a9bfad8851aafe45d70999fd95bf2c816f99
Abdel <112128079+DjeAbdel0@users.noreply.github.com>
2024-03-14 12:45:54 -0400

    swiper_fini_manque_vraie_img_+_verif_ode

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let swiper = new Swiper('.swiper-screenshots', {
-
-      direction: 'horizontal',
-      loop: true,
-      
-      watchSlidesProgress: true,
-      slidesPerView: 2,
-    
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  let swiper = new Swiper('.swiper-screenshots', {
+    direction: 'horizontal',
+    loop: true,
+    watchSlidesProgress: true,
+    slidesPerView: 'auto', 
+    centeredSlides: true, 
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+     
+      1200: {
+        slidesPerView: 3, 
       },
-  
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
+    },
   });
-  
+});
